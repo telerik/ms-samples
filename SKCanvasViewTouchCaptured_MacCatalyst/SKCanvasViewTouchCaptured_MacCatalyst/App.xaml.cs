6f1d859956c586d60422fad6c3b0eecafc042f36
Polya Pelova <pelova@progress.com>
2023-01-23 20:35:57 +0200

    Demonstrates an issue with SKCanvasView on MacCatalyst where the mouse stays captured when Touch is handled by the SKCanvasView.

---
+++ SKCanvasViewTouchCaptured_MacCatalyst/SKCanvasViewTouchCaptured_MacCatalyst/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SKCanvasViewTouchCaptured_MacCatalyst;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SKCanvasViewTouchCaptured_MacCatalyst/SKCanvasViewTouchCaptured_MacCatalyst/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SKCanvasViewTouchCaptured_MacCatalyst;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
