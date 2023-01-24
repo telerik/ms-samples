4efb01b87c669dddbe2a8c077a5a72714c34fe9c
Polya Pelova <pelova@progress.com>
2023-01-24 10:48:03 +0200

    Demonstrates an issue with SKCanvasView on Maui(MacCatalyst) where the mouse stays captured when Touch is handled by the SKCanvasView.

---
+++ Maui/SKCanvasViewTouchCaptured_MacCatalyst/SKCanvasViewTouchCaptured_MacCatalyst/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
+
+namespace SKCanvasViewTouchCaptured_MacCatalyst;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    void SKCanvasView_Touch(System.Object sender, SkiaSharp.Views.Maui.SKTouchEventArgs e)
+    {
+        e.Handled = true;
+    }
+}
+
---
+++ Maui/SKCanvasViewTouchCaptured_MacCatalyst/SKCanvasViewTouchCaptured_MacCatalyst/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SKCanvasViewTouchCaptured_MacCatalyst;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
