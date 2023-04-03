0b852c5c12f25645efeac7d616cb16b356f62749
Atanas Popatanasov <apopatan@progress.com>
2023-04-03 19:19:17 +0300

    Add Label layout issue example project.

---
+++ Maui/LabelLayout/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LabelLayout;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui/LabelLayout/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LabelLayout;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
