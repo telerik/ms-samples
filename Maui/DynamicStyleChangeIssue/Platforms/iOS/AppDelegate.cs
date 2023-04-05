32525f792f777b01f72c7ff1893f266f412fcbee
Atanas Popatanasov <apopatan@progress.com>
2023-04-05 12:32:02 +0300

    Add dynamic style change issue example.

---
+++ Maui/DynamicStyleChangeIssue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DynamicStyleChangeIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
