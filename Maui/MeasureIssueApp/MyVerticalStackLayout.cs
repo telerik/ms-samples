501723baba82a7635d9d4b822456c351fafb437c
Deyan Terziyski <terziysk@progress.com>
2023-04-21 16:19:14 +0300

    Add an app that showcases an issue regarding the MeasureInvalidated event.

---
+++ Maui/MeasureIssueApp/MyVerticalStackLayout.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MeasureIssueApp
+{
+    internal class MyVerticalStackLayout : VerticalStackLayout
+    {
+        // Add a breakpoint inside NotifyMeasureInvalidated and the other 2 methods and then click the button in the MainPage.
+        // The breakpoint is not hit aka the MeasureInvalided is not being Invoked.
+        public MyVerticalStackLayout()
+        {
+            this.MeasureInvalidated += NotifyMeasureInvalidated;
+        }
+        
+        protected override void InvalidateMeasure()
+        {
+            base.InvalidateMeasure();
+        }
+
+        protected override void InvalidateMeasureOverride()
+        {
+            base.InvalidateMeasureOverride();
+        }
+
+        private void NotifyMeasureInvalidated(object sender, EventArgs args)
+        {
+        }
+    }
+}
