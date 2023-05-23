ca404b0e55ceb9e95ea1c44435b891feeed39296
marchev-prgs <petar.marchev@progress.com>
2023-05-23 17:05:55 +0300

    Add an app that demonstrates a memory leak when using Brushes.

---
+++ Maui/BrushesMemoryLeak/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BrushesMemoryLeak;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnClicked(object sender, EventArgs e)
+	{
+		this.Navigation.PushAsync(new GrumpyPage());
+	}
+}
+
