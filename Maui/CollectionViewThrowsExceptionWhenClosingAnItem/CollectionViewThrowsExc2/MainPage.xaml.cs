1374850dbfda79be80a9c0a87dfaf5f8f3f2face
marchev-prgs <petar.marchev@progress.com>
2023-07-24 18:53:19 +0300

    Add an example for an exception in the Maui CollectionView.

---
+++ Maui/CollectionViewThrowsExceptionWhenClosingAnItem/CollectionViewThrowsExc2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewThrowsExc2;
+
+public partial class MainPage : ContentPage
+{
+	private ObservableCollection<string> items = new();
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		this.items.Add("item " + this.items.Count);
+		this.cv1.ItemsSource = this.items;
+	}
+
+	private void ButtonClose_Clicked(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+		string item = (string)button.BindingContext;
+		this.items.Remove(item);
+	}
+}
+
---
+++ Maui/CollectionViewThrowsExceptionWhenClosingAnItem/README.md
@@ -0,0 +1,15 @@
+**Description of the issue**
+Maui: An exception is thrown after an item is removed.
+
+**Steps to reproduce:**
+1. Run the app in WinUI.
+2. Click the "X" button of the item.
+
+**Expected Behavior**
+The item should be removed and the CollectionView should become empty.
+
+**Actual Behavior**
+An exception is thrown.
+
+**Link to issue**
+
