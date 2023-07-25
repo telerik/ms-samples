bf2b23d599ae0575f31f737917b3c45037fb0b39
Ivan Todorov <todorov@progress.com>
2023-07-25 11:41:45 +0300

    Add a sample project for a MAUI issue.
    Evaluating JavaScript in WebView programmatically returns an incorrect result.

---
+++ Maui/WebViewEvaluateJavaScript/TestApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace TestApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+        this.InitializeEditors();
+        this.InitializeWebView();
+    }
+
+    private void InitializeEditors()
+    {
+        this.htmlEditor.Text = """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                </head>
+                <body>
+                    <script>
+                        function test() {
+                            return "Test";
+                        }
+                    </script>
+                    <p>
+                        Hello, World!!!
+                    </p>
+                </body>
+            </html>
+            """;
+
+        this.scriptEditor.Text = "test();";
+    }
+
+    private void InitializeWebView()
+    {
+        this.webView.Source = new TextWebViewSource(this.htmlEditor.Text);
+    }
+
+    private void OnLoadClicked(object sender, EventArgs eventArgs)
+    {
+        this.webView.Source = new TextWebViewSource(this.htmlEditor.Text);
+    }
+
+    private async void OnEvaluateClicked(object sender, EventArgs eventArgs)
+    {
+        this.resultEditor.Text = await this.webView.EvaluateJavaScriptAsync(this.scriptEditor.Text);
+    }
+}
---
+++ Maui/WebViewEvaluateJavaScript/TestApp/TextWebViewSource.cs
@@ -0,0 +1,27 @@
+﻿namespace TestApp;
+
+public class TextWebViewSource : WebViewSource
+{
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(TextWebViewSource));
+
+    public TextWebViewSource()
+    {
+    }
+
+    public TextWebViewSource(string text)
+    {
+        this.Text = text;
+    }
+
+    public string Text
+    {
+        get => (string)this.GetValue(TextProperty);
+        set => this.SetValue(TextProperty, value);
+    }
+
+    public override void Load(IWebViewDelegate webViewDelegate)
+    {
+        webViewDelegate.LoadHtml(this.Text, null);
+    }
+}
