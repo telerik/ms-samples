976869d6bc11137f5b3c0f81e8952669806c73ca
Ivan Todorov <todorov@progress.com>
2023-07-25 11:48:01 +0300

    Add a sample project for a MAUI issue.
    CollectionView leaks memory when ItemsSource has changed.

---
+++ Maui/CollectionViewItemsSourceMemoryLeak/TestApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+        this.BindingContext = new MainViewModel(1, 100);
+    }
+
+    private void OnRecreateClicked(object sender, EventArgs eventArgs)
+    {
+        var controlTemplate = this.contentView.ControlTemplate;
+
+        this.contentView.ControlTemplate = null;
+        this.contentView.ControlTemplate = controlTemplate;
+    }
+}
---
+++ Maui/CollectionViewItemsSourceMemoryLeak/TestApp/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestApp;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected bool SetValue<TValue>(ref TValue oldValue, TValue newValue, [CallerMemberName] string propertyName = null)
+    {
+        var equalityComparer = EqualityComparer<TValue>.Default;
+
+        if (equalityComparer.Equals(oldValue, newValue))
+        {
+            return false;
+        }
+
+        oldValue = newValue;
+
+        this.OnPropertyChanged(propertyName);
+
+        return true;
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
