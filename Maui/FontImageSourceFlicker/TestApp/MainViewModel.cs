63016fea14f8aa33c13b23607fbd60a10c287239
Ivan Todorov <todorov@progress.com>
2023-07-25 11:32:23 +0300

    Add a sample project for a MAUI bug.
    [Android] Changing the ImageSource of an Image causes a noticeable flicker.

---
+++ Maui/FontImageSourceFlicker/TestApp/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestApp;
+
+public class MainViewModel : ViewModelBase
+{
+    private readonly IList<string> colors;
+    private string selectedColor;
+
+    public MainViewModel()
+    {
+        this.colors = new[]
+        {
+            "Black",
+            "Red",
+            "Green",
+            "Blue",
+            "Cyan",
+            "Magenta",
+            "Yellow",
+            "Gray"
+        };
+
+        this.selectedColor = "Black";
+    }
+
+    public IList<string> Colors => this.colors;
+
+    public string SelectedColor
+    {
+        get => this.selectedColor;
+        set
+        {
+            if (this.selectedColor != value)
+            {
+                this.selectedColor = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}
+
