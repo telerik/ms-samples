72cfc6ab5222b0915e30f86969f07a0cc0053135
Atanas Popatanasov <apopatan@progress.com>
2023-08-18 10:31:54 +0300

    Add example about the broken twoway binding in Maui with net8 preview 7.

---
+++ Maui/MauiBindingIssue/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MauiBindingIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        this.BindingContext = new ViewModel();
+	}
+}
+
+public class ViewModel : INotifyPropertyChanged
+{
+    private string myText;
+
+    public ViewModel()
+    {
+        this.MyText = "Hello World";
+        this.ClickCommand = new Command(() => this.MyText = string.Empty);
+    }
+
+    public string MyText
+    {
+        get => myText;
+        set
+        {
+            if (this.myText != value)
+            {
+                this.myText = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+
+    public ICommand ClickCommand { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
