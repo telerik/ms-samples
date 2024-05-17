647197171ac85c4cd8e81988c04900e95bb2616f
marchev-prgs <petar.marchev@progress.com>
2024-05-17 14:34:11 +0300

    Add example for xaml error in VS.

---
+++ Maui/XamlErrorForDataTemplate/XamlErrorForDataTemplate/MyTabViewItem.cs
@@ -0,0 +1,10 @@
+﻿namespace XamlErrorForDataTemplate;
+
+public class MyTabViewItem : BindableObject
+{
+    public static readonly BindableProperty HeaderProperty = BindableProperty.Create("Header", typeof(View), typeof(MyTabViewItem));
+    public static readonly BindableProperty ContentProperty = BindableProperty.Create("Content", typeof(View), typeof(MyTabViewItem));
+
+    public View Header { get { return (View)this.GetValue(HeaderProperty); } set { this.SetValue(HeaderProperty, value); } }
+    public View Content { get { return (View)this.GetValue(ContentProperty); } set { this.SetValue(ContentProperty, value); } }
+}
