e1d74bb2514162c343415c3026b00b7a4ffea42b
Duygu Duran <duran@progress.com>
2024-10-08 17:29:57 +0300

    Add a repro project for images, loaded from stream, not being displayed in Release mode on Android using .NET 9.0.100-rc.1.24452.12 version.

---
+++ Maui/ImagesLoadedFromStreamIssue_Android_Net9/MauiApp5/MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        this.ImageSources = new ObservableCollection<ImageSource>();
+
+        this.InitializeComponent();
+        this.InitImages();
+
+        this.BindingContext = this;
+    }
+
+    public ObservableCollection<ImageSource> ImageSources { get; set; }
+
+    private void InitImages()
+    {
+        var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+        var resourceNames = assembly.GetManifestResourceNames();
+
+        foreach (var resourceName in resourceNames)
+        {
+            if (resourceName.Contains("sample") && resourceName.EndsWith("jpg"))
+            {
+                this.ImageSources.Add(ImageSource.FromStream(() => assembly.GetManifestResourceStream(resourceName)));
+            }
+        }
+    }
+}
