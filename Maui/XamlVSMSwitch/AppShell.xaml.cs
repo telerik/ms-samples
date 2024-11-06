69a6745eadb3e12f8ce7e79c531d3f73afd20eb5
panayot-cankov <panayot.cankov@progress.com>
2024-11-06 20:25:14 +0200

    feat: Add XamlVSMSwtich bug repro

---
+++ Maui/XamlVSMSwitch/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XamlVSMSwitch;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui/XamlVSMSwitch/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace XamlVSMSwitch;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void SetStyle1(object sender, EventArgs e)
+    {
+		if (this.Resources.TryGetValue("Style1", out var res) && res is Style style)
+		{
+			this.Styleable.Style = style;
+		}
+    }
+
+    private void SetStyle2(object sender, EventArgs e)
+    {
+		if (this.Resources.TryGetValue("Style2", out var res) && res is Style style)
+		{
+			this.Styleable.Style = style;
+		}
+    }
+
+	private void SetClearStyle(object sender, EventArgs e)
+    {
+		this.Styleable.Style = null;
+		this.Resources.Remove(typeof(Button).FullName);
+    }
+
+	private void SetStyle1Implicit(object sender, EventArgs e)
+    {
+		this.Resources.Remove(typeof(Button).FullName);
+		if (this.Resources.TryGetValue("Style1", out var res) && res is Style style)
+		{
+			this.Resources.Add(typeof(Button).FullName, style);
+		}
+    }
+
+	private void SetStyle2Implicit(object sender, EventArgs e)
+    {
+		this.Resources.Remove(typeof(Button).FullName);
+		if (this.Resources.TryGetValue("Style2", out var res) && res is Style style)
+		{
+			this.Resources.Add(typeof(Button).FullName, style);
+		}
+    }
+}
+
