573636a4695e104a5dc407bd1f36aac478e70339
Atanas Popatanasov <apopatan@progress.com>
2025-01-22 16:05:49 +0200

    Add sample that demonstrates an issue with Background that is set to a DynamicResource with OnPlatform value.

---
+++ Maui/DynamicBackgroundIssue/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace DynamicBackgroundIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
